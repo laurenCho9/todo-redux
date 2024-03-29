2a53bc877bc101825bb2cdc20de688afe1e49ac3
laurenCho9 <lauren.choux@gmail.com>
2024-03-30 08:04:10 +0900

    feat: redux-persist 연동하기

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import localStorage from "redux-persist/lib/storage";
 import createSagaMiddleware from "redux-saga";
 import rootReducer from "./reducer";
 import rootSaga from "./sagas";
 
+const persistConfig = {
+  key: "root",
+  storage: localStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) => {
     const defaultMiddleware = getDefaultMiddleware();
     return [...defaultMiddleware, sagaMiddleware];
@@ -14,5 +32,7 @@ const store = configureStore({
 });
 
 sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store);
 
 export default store;
